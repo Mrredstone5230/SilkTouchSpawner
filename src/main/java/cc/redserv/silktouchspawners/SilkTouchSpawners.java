fdf242313d8980a96dc76ff2c5fe59217587d2fb
Mr_redstone5230 <sebastienduf@icloud.com>
2020-07-21 10:51:45 -0400

    Initial Commit

---
+++ src/main/java/cc/redserv/silktouchspawners/SilkTouchSpawners.java
@@ -0,0 +1,17 @@
+package cc.redserv.silktouchspawners;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SilkTouchSpawners extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
